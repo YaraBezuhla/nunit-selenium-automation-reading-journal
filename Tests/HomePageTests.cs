e18c2fd868b5bae2824e43a4ef2ef343bfa4fac0
Yaroslava <ybezuhla@gmail.com>
2025-02-22 12:12:32 +0200

    Added test add book by api

---
+++ Tests/HomePageTests.cs
@@ -0,0 +1,19 @@
+﻿
+using nunit_selenium_automation_reading_journal.Pages;
+
+namespace nunit_selenium_automation_reading_journal.Tests
+{
+    [TestFixture]
+    public class HomePageTests : LaunchSettings
+    {
+       
+
+        [Test]
+        public void AssertBlocksTitles()
+        {
+            HomePage homePage = new HomePage(Driver);
+            homePage.AssertBooksTitles("Найпопулярніші книги", "Українські автори");
+
+        }
+    }
+}
