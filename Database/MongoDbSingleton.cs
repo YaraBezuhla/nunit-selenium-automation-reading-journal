5fdb4d2a4cdf539c2cdcb731cfb3a7b9e0a662eb
Yaroslava <ybezuhla@gmail.com>
2025-02-23 17:21:30 +0200

    Added db connect

---
+++ Database/MongoDbSingleton.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+
+namespace nunit_selenium_automation_reading_journal.Database
+{
+    public class MongoDbSingleton
+    {
+        private static readonly Lazy<MongoDbSingleton> instance = 
+            new(() => new MongoDbSingleton());
+
+        private readonly IMongoClient _client;
+        private readonly IMongoDatabase _database;
+
+        private MongoDbSingleton()
+        {
+            var connectionString = Environment.GetEnvironmentVariable("MONGO_DB_CONNECTION")
+                ?? "mongodb://localhost:27017";
+
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase("automationreadingjournal");
+        }
+
+        public static MongoDbSingleton Instance => instance.Value;
+        public IMongoDatabase GetDatabase() => _database;   
+    }
+}
