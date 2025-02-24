45d4953db9975af7cfe1cc1a547fb4b6ec309eb5
Yaroslava <ybezuhla@gmail.com>
2025-02-24 14:57:01 +0200

    Added delete by api

--- Utils/ApiMethods.cs
+++ Utils/ApiMethods.cs
@@ -7,8 +7,9 @@ namespace nunit_selenium_automation_reading_journal.Utils
     public class ApiMethods
     {
         private static readonly string BOOKS_JSON_FILE = "C:/Users/Ярослава/Projects/nunit-selenium-automation-reading-journal/TestData/books.json";
+        private static readonly HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:5000") };
 
-        public async Task AddBookByApi(int bookIndex, int codeResponse)
+public async Task AddBookByApi(int bookIndex, int codeResponse)
         {
             if (!File.Exists(BOOKS_JSON_FILE))
             {
@@ -26,13 +27,43 @@ namespace nunit_selenium_automation_reading_journal.Utils
             JObject selectedBook = (JObject)jArray[bookIndex];
             var json = JsonConvert.SerializeObject(selectedBook, Formatting.Indented); // Серіалізація у JSON-рядок
 
-            using var client = new HttpClient { BaseAddress = new Uri("http://localhost:5000") };
-
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             var responce = await client.PostAsync("/api/books", content);
             Assert.That((int)responce.StatusCode, Is.EqualTo(codeResponse));
 
+        }
+
+        public async Task DeleteBookByApi()
+        {
+            if(!File.Exists(BOOKS_JSON_FILE))
+            {
+                throw new FileNotFoundException("Файл BOOKS_JSON_FILE не знайдено");
+
+            }
+            
+            string jsonContent = await File.ReadAllTextAsync(BOOKS_JSON_FILE);
+            List<Dictionary<string, object>> books = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(jsonContent);
+
+
+            await Task.WhenAll(books.Select(async book =>
+            {
+                string title = book["title"].ToString();
+                try
+                {
+                    var responce = await client.DeleteAsync($"/api/books/title/{title}");
+
+                    if (!responce.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Книга {title} не знайдена в базі даних. Пропущено.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }));
+            
         }
     }
 }
