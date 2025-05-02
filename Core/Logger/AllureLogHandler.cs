e96b6c487d24439ec1140b5db623a737cc6db8b7
Yaroslava <ybezuhla@gmail.com>
2025-05-02 08:20:07 +0300

    Added logger with event

---
+++ Core/Logger/AllureLogHandler.cs
@@ -0,0 +1,33 @@
+﻿using Allure.Net.Commons;
+using OpenQA.Selenium;
+using System.Text;
+
+namespace nunit_selenium_automation_reading_journal.Core.Logger
+{
+    public class AllureLogHandler
+    {
+        private readonly IWebDriver _driver;
+
+        public AllureLogHandler(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Subscribe(TestLogger logger)
+        {
+            logger.OnTestFailed += HandleLog;
+        }
+
+        private void HandleLog(object? sender, TestLogEventArgs e)
+        {
+            string envInfo = $"OS: {Environment.OSVersion}";
+            AllureApi.AddAttachment("Environment Info", "text/plain", Encoding.UTF8.GetBytes(envInfo));
+
+            string errorInfo = $"Status: {e.TestContext.Result.Outcome.Status}\nError: {e.TestContext.Result.Message}\nTime: {e.Duration}";
+            AllureApi.AddAttachment("Test Logs", "text/plain", Encoding.UTF8.GetBytes(errorInfo));
+
+            var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+            AllureApi.AddAttachment("Screenshot", "image/png", screenshot.AsByteArray);
+        }
+    }
+}
---
+++ Core/Logger/TestLogEventArgs.cs
@@ -0,0 +1,15 @@
+﻿
+namespace nunit_selenium_automation_reading_journal.Core.Logger
+{
+    public class TestLogEventArgs : EventArgs
+    {
+        public TestContext TestContext { get; }
+        public TimeSpan Duration { get; }
+
+        public TestLogEventArgs(TestContext testContext, TimeSpan duration)
+        {
+            TestContext = testContext;
+            Duration = duration;
+        }
+    }
+}
