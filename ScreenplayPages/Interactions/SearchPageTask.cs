44064c21896e73d65b1dc73fea5b350fa80b98e1
Yaroslava <ybezuhla@gmail.com>
2025-05-21 13:13:34 +0300

    Practice working with the Screenplay pattern

---
+++ ScreenplayPages/Interactions/SearchPageTask.cs
@@ -0,0 +1,20 @@
+﻿using Boa.Constrictor.Screenplay;
+using Boa.Constrictor.Selenium;
+using nunit_selenium_automation_reading_journal.ScreenplayPages.Pages;
+
+namespace nunit_selenium_automation_reading_journal.ScreenplayPages.Interactions
+{
+    public class SearchPageTask : ITask
+    {
+        public string Phrase { get; }
+
+        private SearchPageTask(string phrase) => Phrase = phrase;
+
+        public static SearchPageTask For(string phrase) => new SearchPageTask(phrase);
+
+        public void PerformAs(IActor actor)
+        {
+            actor.AttemptsTo(SendKeys.To(SearchPage.SearchInput, Phrase));
+        }
+    }
+}
---
+++ ScreenplayPages/Pages/SearchPage.cs
@@ -0,0 +1,20 @@
+﻿
+using Boa.Constrictor.Selenium;
+using OpenQA.Selenium;
+using static Boa.Constrictor.Selenium.WebLocator;
+
+namespace nunit_selenium_automation_reading_journal.ScreenplayPages.Pages
+{
+    public static class SearchPage
+    {
+        public const string Url = "http://localhost:8080/search";
+
+        public static IWebLocator SearchInput => L(
+            "Books Search Input",
+            By.ClassName("search-input"));
+
+        public static IWebLocator BookTitle => L(
+            "Book Title",
+            By.XPath("//h3[@data-test='search-book-title']"));
+    }
+}
